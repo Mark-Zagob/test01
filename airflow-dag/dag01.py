9ed38c4021de90b95dab5a14913d9e3a36bf1ccf
dungtt <dungtt@thuongpro.vn>
2024-03-19 09:46:00 +0000

    change

--- airflow-dag/dag01.py
+++ airflow-dag/dag01.py
@@ -20,8 +20,8 @@ def _get_pictures():
     pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
     subprocess.run(["pwd"], capture_output=True)
     print(subprocess.run(["pwd"], capture_output=True))
-    subprocess.run(["ls", " -la"], capture_output=True)
-    print(subprocess.run(["ls", " -la"], capture_output=True))
+    subprocess.run(["ls"], capture_output=True)
+    print(subprocess.run(["ls"], capture_output=True))
     # with open("launches.json") as f:
     #     launches= json.load(f)
     #     image_urls =[launch["image"] for launch in launches["results"]]
