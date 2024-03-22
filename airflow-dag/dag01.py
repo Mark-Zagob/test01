83bd5c8316183709d71f9f0a5c5c81ea843e6995
dungtt <dungtt@thuongpro.vn>
2024-03-22 05:58:15 +0000

    change

--- airflow-dag/dag01.py
+++ airflow-dag/dag01.py
@@ -1,4 +1,4 @@
-import json, pathlib, airflow, requests, os, subprocess
+import json, pathlib, airflow, requests, os, subprocess, urllib.request
 import requests.exceptions as requests_exceptions
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -12,14 +12,17 @@ dag = DAG(
 )
 download = BashOperator(
     task_id="download",
-    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming' && cd /tmp/ && ls -la && pwd",
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming' && pwd && ls -la && cd /tmp/ && ls -la && pwd",
     dag = dag,
 )
 
 def _get_pictures():
+    urllib.request.urlretrieve(url="https://ll.thespacedevs.com/2.0.0/launch/upcoming", filename="launches.json")
     pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
     subprocess.run(["pwd"], capture_output=True)
     print(subprocess.run(["pwd"], capture_output=True))
+    subprocess.run(["ls", "-la"], capture_output=True)
+    print(subprocess.run(["ls", "-la"], capture_output=True))
     subprocess.run(["ls", "/tmp"], capture_output=True)
     print(subprocess.run(["ls", "/tmp"], capture_output=True))
     # with open("launches.json") as f:
