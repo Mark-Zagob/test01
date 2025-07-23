2a87af5b9fffb8dc9c9b0d57fea843642edeff54
tud7hc <dungtt@vtdc.local>
2025-07-23 10:27:36 +0700

    test: add security scan workflow in develop branch

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# test01
-just testing
+## Description
+Sample security CI workflows 
+
+## Feature
+- Using security tools to scan and display vulnerability
+- Automatic create issue and block PR if job failed
---
+++ python_codes/app.py
@@ -0,0 +1,24 @@
+import logging
+from flask import Flask, jsonify
+
+# Khởi tạo Flask app
+app = Flask(__name__)
+
+# Cấu hình logging
+logging.basicConfig(
+    level=logging.INFO,  # Mức độ log (DEBUG, INFO, WARNING, ERROR)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+
+# Tạo logger riêng
+logger = logging.getLogger(__name__)
+
+@app.route('/')
+def hello_world():
+    logger.info("Truy cập endpoint /")
+    return jsonify(message='Test, Merge')
+
+@app.route('/error')
+def trigger_error():
+    logger.error("Đã xảy ra lỗi mẫu!")
+    return jsonify(error="This is a sample error"), 500
